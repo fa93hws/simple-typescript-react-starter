bc5c3a70df24094fd695ac7002caba1933ffdedd
Eric <eric@canva.com>
2019-07-14 12:09:40 +1000

    remove unused

--- conf/css-module-types-loader.js
+++ conf/css-module-types-loader.js
@@ -1,6 +1,6 @@
 module.exports = function cssModuleLoader(source) {
   const styles = this.exec(source, this.resource);
-  const styleMap = `{ ${Object.keys(styles).map(p => `${p}: ${JSON.stringify(styles[p])} as string`).join(',')} }`;
+  const styleMap = `{ ${Object.keys(styles).map(key => `${key}: ${styles[key]}`).join(',')} }`;
 
   const result = `
 const styles = ${styleMap};
