8933f7d3542eaa0182901d8e2a1fe4ec17df5001
Eric <eric@canva.com>
2019-07-14 12:46:07 +1000

    Fix css types loader

--- conf/css-module-types-loader.js
+++ conf/css-module-types-loader.js
@@ -1,6 +1,6 @@
 module.exports = function cssModuleLoader(source) {
   const styles = this.exec(source, this.resource);
-  const styleMap = `{ ${Object.keys(styles).map(key => `${key}: ${styles[key]}`).join(',')} }`;
+  const styleMap = `{ ${Object.keys(styles).map(key => `${key}: ${JSON.stringify(styles[key])}`).join(',')} }`;
 
   const result = `
 const styles = ${styleMap};
